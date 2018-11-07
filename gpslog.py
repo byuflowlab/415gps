b2e2b79e7ae5c39b066b0768242a03831b20df85
Taylor McDonnell <taylor.golden.mcdonnell@gmail.com>
2018-11-06 17:50:25 -0700

    now works with python 2 and 3

--- gpslog.py
+++ gpslog.py
@@ -8,10 +8,24 @@ import warnings
 import math
 
 # user defined function
+
+
 def usrfun(time, fix, NumSat, lat, lon, alt, speed, ground_course, covariance):
-    # YOUR CODE GOES HERE
-    print(time, fix, NumSat, lat, lon, alt, speed, ground_course, covariance)
+
+    # modify the contents of this function to fit your needs
+
+    if usrfun.counter%10 == 0:
+        print("time,fix,NumSat,lat,lon,alt,speed,ground_course,covariance")
+
+    print(time,fix,NumSat,lat,lon,alt,speed,ground_course,covariance)
+
+    usrfun.counter += 1
+
+    # we return None because nothing is done with the output of this function
     return None
+
+# you can define variables outside of your function like this and use them inside
+usrfun.counter = 0
 
 # --- DO NOT MODIFY ANYTHING BELOW THIS LINE --- #
 
@@ -21,6 +35,7 @@ import libnmea_navsat_driver.driver
 # parse arguments
 if len(sys.argv) < 2:
     print('Please specify the port (e.g. python gpslog.py /dev/ttyUSB0)')
+    sys.exit(0)
 else:
     serial_port = sys.argv[1]
 
@@ -28,10 +43,10 @@ else:
 GPS = serial.Serial(port=serial_port, baudrate=57600, timeout=2)
 driver = libnmea_navsat_driver.driver.NMEADriver()
 
-# set filename and open file
+# set filename and open csv file
 timestr = time.strftime("%Y%m%d-%H%M%S.csv")
-f = open(timestr, "w")
-f.write("time,fix,NumSat,latitude,longitude,altitude,speed,ground_course,covariance\n")
+csvfile = open(timestr, "w")
+csvfile.write("time,fix,NumSat,latitude,longitude,altitude,speed,ground_course,covariance\n")
 
 # set time to zero
 t0 = time.time()
@@ -40,15 +55,15 @@ try:
     while True:
         data = GPS.readline().strip()
         try:
-            out = driver.add_sentence(data, time.time()-t0)
+            out = driver.add_sentence(data.decode(), time.time()-t0)
             if out != False:
                 usrfun(*out)
                 if not (math.isnan(out[3]) or math.isnan(out[4]) or math.isnan(out[5])):
-                    f.write(str(out[0])+","+str(out[1])+","+str(out[2])+","+ \
+                    csvfile.write(str(out[0])+","+str(out[1])+","+str(out[2])+","+ \
                         str(out[3])+","+str(out[4])+","+str(out[5])+","+ \
                         str(out[6])+","+str(out[7])+","+str(out[8])+"\n")
         except ValueError as e:
-            warnings.warn("Value error, likely due to missing fields in the NMEA message. Error was: %s. Please report this issue at github.com/ros-drivers/nmea_navsat_driver, including a bag file with the NMEA sentences that caused it." % e)
+            warnings.warn("Value error, likely due to missing fields in the NMEA message. Error was: %s. Please report this issue at github.com/ros-drivers/nmea_navsat_driver, including a bag csvfile with the NMEA sentences that caused it." % e)
 except KeyboardInterrupt:
     GPS.close() #Close GPS serial port
-    f.close()
+    csvfile.close() # Close CSV file
